80615dff07f1d27bb7e785426978702fab03d482
Minjoo Kim <sjctk1@gmail.com>
2025-02-06 21:40:58 +0900

    Add XSS detection code

--- src/main/java/TrafficAnomalyDetection/FDS/AnomalyDetection/NetworkFlow.java
+++ src/main/java/TrafficAnomalyDetection/FDS/AnomalyDetection/NetworkFlow.java
@@ -13,9 +13,12 @@ public class NetworkFlow {
 		 System.out.println(jsonDataArray.length());
 		 System.out.println(jsonDataArray.getJSONObject(0));
 		 
-		 if (abnormalType == "SQLInjection") {
-			 NetworkFlow networkFlow = new NetworkFlow();
+		 NetworkFlow networkFlow = new NetworkFlow();
+		 
+		 if (abnormalType == "SQLDetection") {
 			 networkFlow.SQLInjectionDetect(jsonDataArray);
+		 } else if (abnormalType == "XSSInjection") {
+			 networkFlow.XSSDetection(jsonDataArray);
 		 } else {
 			System.out.println("No proper detection type!");
 		 }
@@ -43,5 +46,30 @@ public class NetworkFlow {
                 }
             }
 	     }
+	 }
+	 
+	 public void XSSDetection(JSONArray jsonDataArray) {
+		 String[] xssPatterns = {
+	                "<script>", "</script>", "javascript:", "onerror=", "onload=",
+	                "alert(", "document.cookie", "document.write", "eval(", "href="
+	        };
+	     Pattern xssPattern = Pattern.compile(String.join("|", xssPatterns), Pattern.CASE_INSENSITIVE);
+	     
+	     for (int i = 0; i < jsonDataArray.length(); i++) {
+	    	 JSONObject packet = jsonDataArray.getJSONObject(i);
+	    	 JSONObject data = packet.getJSONObject("data");
+	    	 JSONObject layers = data.getJSONObject("layers");
+	    	 
+	    	// HTTP 요청이 포함된 패킷인지 확인
+            if (layers.has("http") && layers.getJSONObject("http").has("http.request.uri")) {
+                String uri = layers.getJSONObject("http").getString("http.request.uri");
+
+                // XSS 패턴이 포함되어 있는지 검사
+                if (xssPattern.matcher(uri).find()) {
+                    System.out.println("🚨 Potential XSS Attack detected: " + uri);
+                }
+            }
+	     }
+
 	 }
 };
