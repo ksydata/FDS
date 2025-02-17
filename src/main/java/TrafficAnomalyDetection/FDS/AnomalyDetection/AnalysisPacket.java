eedb85641a4472d8d1f85113c70933e102d2ff6f
ksydata <ksydata01@gmail.com>
2025-02-17 22:43:38 +0900

    trial & error 1 of defining HTTP protocol scan

--- src/main/java/TrafficAnomalyDetection/FDS/AnomalyDetection/AnalysisPacket.java
+++ src/main/java/TrafficAnomalyDetection/FDS/AnomalyDetection/AnalysisPacket.java
@@ -1,11 +1,47 @@
 package TrafficAnomalyDetection.FDS.AnomalyDetection;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class AnalysisPacket extends AnomalyDetection {
 	@Override
 	public void executeDetection(JSONArray jsonDataArray) {
-		System.out.print("IP 주소 기반 이상행위, Port 스캔 공격 탐지");
-		// 패킷 크기/빈도(타이밍) 분석, 비정상 시그니처 탐지
-	}
+		for (int index = 0; index < jsonDataArray.length(); index++) {
+			// JSON 배열 내 index 번째 document(패킷)을 JSON 문자열 형식으로 변환 
+			JSONObject packet = jsonDataArray.getJSONObject(index);
+            // System.out.println("Packet at index " + index + ": " + packet.toString());
+			
+			// _source 필드 layers 아래에 패킷의 구조 정의(이더넷, IP, TCP, TLS 등)
+            if (packet.has("data")) {
+                JSONObject source = packet.getJSONObject("data");
+                JSONObject layers = source.getJSONObject("layers");
+			// JSONObject layers = packet.getJSONObject("_source").getJSONObject("layers");
+		            
+				// HTTP 패킷 여부 확인
+				if (layers.has("http")) {
+	                JSONObject httpLayer = layers.getJSONObject("http");
+					
+					// 1. HTTP 메서드 확인
+					if (httpLayer.has("http.request.method")) {
+						String method = httpLayer.getString("http.request.method");
+						System.out.println("HTTP Method: " + method);
+					}
+				
+					// 2. HTTPS 적용 여부
+	                /*
+					if (packet.getJSONObject("_source").getJSONObject("layers").has("ssl")) {
+	                	System.out.println("SSL/TLS is enabled");
+	                }              } else {
+	                    System.out.println("Warning: HTTP traffic without SSL/TLS.");
+	                */  
+					// 3. HTTP 헤더 분석
+					
+					// 4. HTTP 요청 패킷 내 Payload 검사(GET, POST 파라미터에서 SQL 인젝션, XSS 공격패턴 탐지 목적)
+	                    
+				}
+            } else {
+                System.out.println("No '_source' field found for packet");
+            }
+        }
+    }
 }
--- src/main/java/TrafficAnomalyDetection/FDS/AnomalyDetection/AnomalyDetectionFactory.java
+++ src/main/java/TrafficAnomalyDetection/FDS/AnomalyDetection/AnomalyDetectionFactory.java
@@ -10,24 +10,12 @@ public class AnomalyDetectionFactory {
 		
 		// 실행 클래스에서 scanner를 통해 입력받은 detectionType에 따른 구체 클래스 반환
 		if (detectionType.equalsIgnoreCase("IP_PORT_SCAN")) {
-			// "PACKET_PATTERN", ...
 			return new AnalysisPacket();
 		} else if (detectionType.equalsIgnoreCase("DDoS")) {
 			return new DDoSDetection();
-		} else if (detectionType.equalsIgnoreCase("SQLDetection")) {
-			return new SQLInjectionDetection();
-		 } else if (detectionType.equalsIgnoreCase("XSSInjection")) {
-			return new XSSDetection();
-		 } else if (detectionType.equalsIgnoreCase("Dridex")) {
-			 return new DridexDetection();
-		 } else if (detectionType.equalsIgnoreCase("SYNFlood")) {
-			 return new SYNFloodDetection();
-		 } else if (detectionType.equalsIgnoreCase("QUIC")) {
-			 return new QUICDetection();
-		 } else {
+		} else {
 			System.out.println("No proper detection type!");
-		 }
-		
+		}
 		// detectionType이 지정되지 않은 탐지 로직일 경우 null값 반환
 		return null;
 	}
